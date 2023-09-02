39a1dcdc8095bae038e902fe82837165bf76b947
xsharawi <mohammadsharawi14@gmail.com>
2023-09-02 11:20:42 +0300

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-import * as aws from "@aws-sdk/client-rekognition"
-console.log(aws)
+import { CreateCollectionCommand} from  "@aws-sdk/client-rekognition";
+import  { RekognitionClient } from "@aws-sdk/client-rekognition";
+import {fromIni} from '@aws-sdk/credential-providers';
+
+// Set the AWS Region.
+const REGION = "eu-central-1"; //e.g. "us-east-1"
+// Set the profile name
+const profileName = "sharawiProfile"
+// Name the collection
+const collectionName = "firstcollection"
+const rekogClient = new RekognitionClient({region: REGION, 
+  credentials: fromIni({profile: profileName,}), 
+});
+
+const createCollection = async (collectionName) => {
+    try {
+       console.log(`Creating collection: ${collectionName}`)
+       const data = await rekogClient.send(new CreateCollectionCommand({CollectionId: collectionName}));
+       console.log("Collection ARN:")
+       console.log(data.CollectionARN)
+       console.log("Status Code:")
+       console.log(String(data.StatusCode))
+       console.log("Success.",  data);
+       return data;
+    } catch (err) {
+      console.log("Error", err.stack);
+    }
+  };
+
+ createCollection(collectionName)
